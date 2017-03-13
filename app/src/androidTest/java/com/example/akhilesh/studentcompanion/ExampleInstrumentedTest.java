67717d281f58ce7a2e485f352205e54115888ab7
akhilesh96 <akhileshgundala@gmail.com>
2017-03-13 22:47:37 +0530

    second test commit

--- app/src/androidTest/java/com/example/akhilesh/studentcompanion/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/akhilesh/studentcompanion/ExampleInstrumentedTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- * just for test
+ * just for test bc
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
